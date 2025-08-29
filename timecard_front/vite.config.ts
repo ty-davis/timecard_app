e65108db7d329adb01bf096070b09cf0758db822
Ty Davis <tydavis.cougarfan@gmail.com>
2025-08-29 16:00:20 -0600

    Added support for env variable for the backend address

--- timecard_front/vite.config.ts
+++ timecard_front/vite.config.ts
@@ -1,27 +1,30 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-    tailwindcss(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    plugins: [
+      vue(),
+      vueDevTools(),
+      tailwindcss(),
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      },
     },
-  },
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://127.0.0.1:5000',
-        changeOrigin: true,
+    server: {
+      proxy: {
+        '/api': {
+          target: env.VITE_API_TARGET || 'http://127.0.0.1:5000',
+          changeOrigin: true,
+        }
       }
     }
   }
