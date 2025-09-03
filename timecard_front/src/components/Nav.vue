0a4dfc8c72cf526ec1e7b3aec899c2856cea7835
Ty Davis <tydavis.cougarfan@gmail.com>
2025-09-03 13:33:38 -0600

    Updated the Nav to be responsive to mobile

--- timecard_front/src/components/Nav.vue
+++ timecard_front/src/components/Nav.vue
@@ -7,19 +7,27 @@ const auth = useAuthStore();
 
 const visible = ref(false);
 
+const closeDrawer = () => { visible.value = false };
+
 </script>
 
 <template>
   <nav>
-    <div class="navbar">
-      <Drawer v-model:visible="visible" header="Menu" position="right">
+    <div class="navbar flex items-baseline sm:mb-2">
+      <Drawer v-model:visible="visible" header="Menu" position="right" class="sm:hidden">
         <div class="flex flex-col gap-3">
-          <RouterLink v-if="auth.isLoggedIn" to="/report">Report</RouterLink>
+          <RouterLink v-if="auth.isLoggedIn" to="/report" @click="closeDrawer">Report</RouterLink>
           <Button v-if="auth.isLoggedIn" @click="auth.logout">Logout</Button>
         </div>
       </Drawer>
       <RouterLink to="/" class="page-title">Timecard</RouterLink>
-      <Button icon="pi pi-bars" @click="visible = !visible" link/>
+      <div class="hidden sm:flex gap-2">
+        <Button text><RouterLink v-if="auth.isLoggedIn" to="/report">Report</RouterLink></Button>
+        <Button v-if="auth.isLoggedIn" @click="auth.logout">Logout</Button>
+      </div>
+      <div class="block sm:hidden">
+        <Button icon="pi pi-bars" @click="visible = !visible" link/>
+      </div>
     </div>
   </nav>
 </template>
